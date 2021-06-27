fbaa2c0a63e9befba4c8dc572f8f3065e39d9e73
Marko Tepeluk <martepeluk@gmail.com>
2021-06-27 23:25:40 +0200

    Added sharps and flats as radio buttons

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Chord from './chords/chord';
 
-const noteValues: Record<number, string> = {
+const noteValueToString: Record<number, string> = {
   0: 'C',
   1: 'C#',
   2: 'D',
@@ -17,23 +17,47 @@ const noteValues: Record<number, string> = {
   11: 'H'
 }
 
-class App extends React.Component<{}, {notes: string[]}> {
+const noteStringToValue: Record<string, number> = {
+  'C': 0,
+  'C#': 1,
+  'D': 2,
+  'D#': 3,
+  'E': 4,
+  'F': 5,
+  'F#': 6,
+  'G': 7,
+  'G#': 8,
+  'A': 9,
+  'A#': 10,
+  'H': 11
+}
+
+class App extends React.Component<{}, {notes: string[], sharp: number}> {
   constructor(props: any) {
     super(props);
     this.state = {
-      notes: []
+      notes: [],
+      sharp: 0
     };
     this.onChangeNote = this.onChangeNote.bind(this);
+    this.onChangeSharp = this.onChangeSharp.bind(this);
   }
 
   onChangeNote(event: any) {
     var noteValue: number = +event.target.value;
-    console.log(noteValues[noteValue]);
-    console.log(noteValues[(noteValue + 4) % 12]);
-    console.log(noteValues[(noteValue + 7) % 12]);
-    console.log(noteValues[(noteValue + 11) % 12]);
-    var noteList : any = [noteValues[noteValue], noteValues[(noteValue + 4) % 12], noteValues[(noteValue + 7) % 12], noteValues[(noteValue + 11) % 12]]
-    this.setState({notes : noteList});
+    let noteList: string[] = this.calculateChordFromBaseNote(noteValue);
+    this.setState({notes : noteList, sharp : this.state.sharp});
+  }
+
+  onChangeSharp(event: any) {
+    var sharpValue: number = +event.target.value;
+    let noteList: string[] = this.calculateChordFromBaseNote(noteStringToValue[this.state.notes[0]] + sharpValue);
+    this.setState({notes : noteList, sharp : this.state.sharp});
+  }
+
+  calculateChordFromBaseNote(baseNote: number): string[] {
+    var noteStringList: string[] = [noteValueToString[baseNote % 12], noteValueToString[(baseNote + 4) % 12], noteValueToString[(baseNote + 7) % 12], noteValueToString[(baseNote + 11) % 12]];
+    return noteStringList;
   }
 
   render() {
@@ -47,6 +71,12 @@ class App extends React.Component<{}, {notes: string[]}> {
           <input type="radio" value="7" name="note" /> G
           <input type="radio" value="9" name="note" /> A
           <input type="radio" value="11" name="note" /> H
+        </div>
+
+        <div onChange={this.onChangeSharp}>
+          <input type="radio" value="1" name="sharp" /> #
+          <input type="radio" value="0" name="sharp" /> none
+          <input type="radio" value="-1" name="sharp" /> b
         </div>
 
         <Chord notes={this.state.notes}/>
